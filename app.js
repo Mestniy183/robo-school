fd5bbf0ab71f87d4f29519f85fbac17b6ac3fd30
Mestniy183 <ken-zero@ya.ru>
2026-04-10 11:49:14 +0300

    fix burger

--- app.js
+++ app.js
@@ -25,4 +25,21 @@ var swiper = new Swiper(".swiper", {
   burgerElement.addEventListener('click', () => {
     itemsElement.classList.toggle('open')
     burgerElement.classList.toggle('open')
+    if(itemsElement.classList.contains('open')){
+        document.documentElement.style.overflow = 'hidden'
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.documentElement.style.overflow = ''
+        document.body.style.overflow = ''
+    }
+  })
+
+  const mobileLinks = document.querySelectorAll('.header__link');
+  mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        itemsElement.classList.remove('open')
+        burgerElement.classList.remove('open')
+        document.documentElement.style.overflow = ''
+        document.body.style.overflow = ''
+    })
   })
